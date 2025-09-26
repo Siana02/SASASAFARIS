e01c29b0fb1bc071d89ba8408b2d8a1fed67fdcc
Siana <160731655+Siana02@users.noreply.github.com>
2025-09-26 14:51:01 +0300

    Import style.css in index.js

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './styles/style.css';
 import './index.css';
 import App from './App.jsx';
 import reportWebVitals from './reportWebVitals';
