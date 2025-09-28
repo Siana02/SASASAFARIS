d447956b6acab97695e1a038636699f01038eaf0
Siana <160731655+Siana02@users.noreply.github.com>
2025-09-28 14:59:07 +0300

    Add Contact component with subcomponents

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,1 +1,15 @@
+import ContactHours from "./ContactHours";
+import ContactFormSection from "./ContactFormSection";
+import ContactSocials from "./ContactSocials";
+import ContactLocation from "./ContactLocation";
 
+export default function Contact() {
+  return (
+    <div className="contact-page">
+      <ContactHours />    {/* Only on contact page! */}
+      <ContactFormSection />
+      <ContactSocials />
+      <ContactLocation />
+    </div>
+  );
+}
