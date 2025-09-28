429a0dbac1e06a2549ccb1e8150badce162e551a
Siana <160731655+Siana02@users.noreply.github.com>
2025-09-28 17:04:42 +0300

    Implement scroll to top on Contact component mount
    Added useEffect to scroll to top on mount.

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,24 +1,20 @@
-import React from "react";
-import Header from "./Header";
-import MobileNav from "./MobileNav";
-import Footer from "./Footer";
+import React, { useEffect } from "react";
 import ContactHours from "./ContactHours";
 import ContactFormSection from "./ContactFormSection";
 import ContactSocials from "./ContactSocials";
 import ContactLocation from "./ContactLocation";
 
 export default function Contact() {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
   return (
-    <>
-      <Header />
-      <MobileNav />
-      <main className="contact-page">
-        <ContactHours />          {/* Only visible on Contact page */}
-        <ContactFormSection />
-        <ContactSocials />
-        <ContactLocation />
-      </main>
-      <Footer />
-    </>
+    <main className="contact-page">
+      <ContactHours />          {/* Only visible on Contact page */}
+      <ContactFormSection />
+      <ContactSocials />
+      <ContactLocation />
+    </main>
   );
 }
