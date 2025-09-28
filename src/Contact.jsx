50ac29fde0d1889a97ec779946a10da1ae28aea0
Siana <160731655+Siana02@users.noreply.github.com>
2025-09-28 15:05:06 +0300

    Create Contact component with layout and sections

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -1,1 +1,22 @@
+import React from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import ContactHours from "./components/ContactHours";
+import ContactFormSection from "./components/ContactFormSection";
+import ContactSocials from "./components/ContactSocials";
+import ContactLocation from "./components/ContactLocation";
 
+export default function Contact() {
+  return (
+    <>
+      <Header />
+      <main className="contact-page">
+        <ContactHours />          {/* Only visible on Contact page */}
+        <ContactFormSection />
+        <ContactSocials />
+        <ContactLocation />
+      </main>
+      <Footer />
+    </>
+  );
+}
