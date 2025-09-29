629693fbcd93165f892cdb14f52952046ecd9b89
Siana <160731655+Siana02@users.noreply.github.com>
2025-09-29 10:45:00 +0300

    Fix typo in ComingSoon component name

--- src/App.jsx
+++ src/App.jsx
@@ -35,7 +35,7 @@ function HomePage() {
         <ReviewSection />
         <ContactFormSection />
         <ContactSocials />
-        <Comingsoon />
+        <ComingSoon />
         <HighlightSection />
       </main>
       
